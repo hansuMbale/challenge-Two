ca146aced4b4087f1ea5f2097c1b30f8d535c00f
nazirini <nazirinisiraji@gmail.com>
2018-07-16 08:30:58 +0300

    changedd tests file

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,23 @@
 from flask import Flask
 import json
-from .data import BaseTestCase
+import unittest 
 
+app = Flask(__name__)
+
+class BaseTestCase(unittest.TestCase):
+    def setUp(self):
+        self.request = {
+        'fullname': 'nazirini ',
+        'email': 'hansu@gmail.com',
+        'phone': '536780',
+        'department': 'Team Lead',
+        'computerID': 'AAA53',
+        'description': 'Screen freezes when handling heavy apps'}
+        self.content_type = "application/json"
+        self.test_client = app.test_client()
+
+    def tearDown(self):
+        pass
 
 class RequestTestCase(BaseTestCase):
     
